b4a788cb7f313f8523ffd880b82956cfe3d93e8c
ArtHarutyunyan <55248428+ArtHarutyunyan@users.noreply.github.com>
2023-01-12 01:36:47 +0400

    useNavigate, useLocation

--- src/components/SinglePage.jsx
+++ src/components/SinglePage.jsx
@@ -1,9 +1,13 @@
-import { useParams, Link } from "react-router-dom";
+import { useParams, Link, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 
 const SinglePage = () => {
   const { id } = useParams();
   const [post, setPost] = useState(null);
+  const navigate = useNavigate();
+
+  const goBack = () => navigate(-1);
+  const goHome = () => navigate("/", { replace: true });
 
   useEffect(() => {
     fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
@@ -14,6 +18,9 @@ const SinglePage = () => {
     <div>
       {post && (
         <>
+          <button onClick={goBack}>Go Back</button>
+          {/*Bad approach, use Link*/}
+          <button onClick={goHome}>Go Home</button>
           <h1>{post.title}</h1>
           <p>{post.body}</p>
           <Link to={`/posts/${id}/edit`}>Edit this post</Link>
