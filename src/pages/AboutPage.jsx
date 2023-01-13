18886422ed941fb0d1efac13b7b303995090b731
ArtHarutyunyan <55248428+ArtHarutyunyan@users.noreply.github.com>
2023-01-13 15:03:20 +0400

    add contacts and team links

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,8 +1,24 @@
+import { Link, Outlet, Route, Routes } from "react-router-dom";
+
 const AboutPage = () => {
   return (
     <div>
       <h1>About us</h1>
       <p>This is a demo website about React-router-dom library.</p>
+      <ul>
+        <li>
+          <Link to="contacts">Our contact</Link>
+        </li>
+        <li>
+          <Link to="team">Our team</Link>
+        </li>
+      </ul>
+
+      {/* <Routes>
+        <Route path="contacts" element={<p>Our contact</p>} />
+        <Route path="team" element={<p>Our team</p>} />
+      </Routes> */}
+      <Outlet />
     </div>
   );
 };
